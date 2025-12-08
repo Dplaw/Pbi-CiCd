5c43b69bdefba17e27aa9a7bd64b527cb32b1e01
Daniel Plawszewski <dplawszewski1@gmail.com>
2025-12-08 12:58:52 +0100

    changed paramiter in query

--- scripts/report_creator.py
+++ scripts/report_creator.py
@@ -28,15 +28,8 @@ def _create_or_update_model(template: PowerBiTemplateConfig, plan: ExpectedPbiRe
 
     shutil.copytree(template.template_model, plan.expected_model_path, dirs_exist_ok=True)
 
-    platform = _load_json(plan.model_platform)
-    platform["config"]["displayName"] = plan.report_name
-    platform["metadata"]["displayName"] = plan.report_name
-
-    if existing_logical_id:
-        platform["config"]["logicalId"] = existing_logical_id
-    else:
-        platform["config"]["logicalId"] = _generate_region_logical_id(plan, kind="model")
-    _save_json(plan.model_platform, platform)
+    get_update_model_platform(plan, existing_logical_id)
+    get_update_model_definition(template, plan)
 
 
 def _create_or_update_report(template: PowerBiTemplateConfig, plan: ExpectedPbiReportInfo) -> None:
@@ -47,21 +40,11 @@ def _create_or_update_report(template: PowerBiTemplateConfig, plan: ExpectedPbiR
         existing_logical_id = old_platform.get("config", {}).get("logicalId")
 
     shutil.copytree(template.template_report, plan.expected_report_path, dirs_exist_ok=True)
-
-    platform = _load_json(plan.expected_report_platform)
-    platform["config"]["displayName"] = plan.report_name
-    platform["metadata"]["displayName"] = plan.report_name
-
-    if existing_logical_id:
-        platform["config"]["logicalId"] = existing_logical_id
-    else:
-        platform["config"]["logicalId"] = _generate_region_logical_id(plan, kind="report")
-
-    _save_json(plan.expected_report_platform, platform)
+    get_update_report_platform(plan, existing_logical_id)
 
     if hasattr(plan, "expected_report_definition"):
         _update_report_definition(plan)
-
+ 
 
 def _load_json(path: Path | str) -> Dict[str, Any]:
     with path.open("r", encoding="utf-8") as f:
@@ -76,6 +59,36 @@ def _save_json(path, data):
 def _generate_region_logical_id(plan: ExpectedPbiReportInfo, kind: str) -> str:
     name_for_id = f"{kind}:{plan.region_code}"
     return str(uuid.uuid5(LOGICAL_ID_NAMESPACE, name_for_id))
+
+
+def get_update_model_platform(plan: ExpectedPbiReportInfo, existing_logical_id: str) -> None:
+    platform = _load_json(plan.model_platform)
+    platform["config"]["displayName"] = plan.report_name
+    platform["metadata"]["displayName"] = plan.report_name
+    if existing_logical_id:
+        platform["config"]["logicalId"] = existing_logical_id
+    else:
+        platform["config"]["logicalId"] = _generate_region_logical_id(plan, kind="model")
+    _save_json(plan.model_platform, platform)
+
+
+def get_update_model_definition(template: PowerBiTemplateConfig, plan: ExpectedPbiReportInfo) -> None:
+    with plan.model_definition.open("r", encoding="utf-8") as f:
+        txt = f.read()
+    txt = txt.replace(template.template_model_parameter, plan.region_code)
+    with plan.model_definition.open("w", encoding="utf-8") as f:
+        f.write(txt)
+
+
+def get_update_report_platform(plan: ExpectedPbiReportInfo, existing_logical_id: str) ->None:
+    platform = _load_json(plan.expected_report_platform)
+    platform["config"]["displayName"] = plan.report_name
+    platform["metadata"]["displayName"] = plan.report_name
+    if existing_logical_id:
+        platform["config"]["logicalId"] = existing_logical_id
+    else:
+        platform["config"]["logicalId"] = _generate_region_logical_id(plan, kind="report")
+    _save_json(plan.expected_report_platform, platform)
 
 
 def _update_report_definition(plan: ExpectedPbiReportInfo) -> None:
